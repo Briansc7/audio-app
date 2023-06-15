6d3d9d48c9fccdc843fd17f85a615d02c6d22929
Briansc7 <samudio.utn@gmail.com>
2023-06-15 16:38:33 -0300

    added initial home page, routes management and initial slice management with store

---
+++ src/routes/routes.js
@@ -0,0 +1,14 @@
+import HomeContainer from "../components/pages/home/HomeContainer";
+
+export const menuRoutes = [
+  {
+    id: "home",
+    path: "/",
+    Element: HomeContainer,
+  },
+  /* {
+    id: "login",
+    path: "/login",
+    Element: LoginContainer,
+  }, */
+];

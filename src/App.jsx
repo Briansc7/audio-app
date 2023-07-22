076e5c954ed6fc0d4f9f42489f23785b8418142f
Briansc7 <samudio.utn@gmail.com>
2023-07-21 21:00:22 -0300

    added integration with cloudinary to upload images on register

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import AppRouter from "./routes/AppRouter";
 import { ThemeProvider } from "@emotion/react";
 import { customTheme } from "./themeConfig";
 
+import { CloudinaryContext } from "cloudinary-react";
+
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <AppRouter />
+        <CloudinaryContext cloudName="di9tla2mu">
+          <AppRouter />
+        </CloudinaryContext>
       </ThemeProvider>
     </BrowserRouter>
   );
--- src/components/pages/register/Register.jsx
+++ src/components/pages/register/Register.jsx
@@ -1,13 +1,35 @@
 import { TextField } from "@mui/material";
+import { Image } from "cloudinary-react";
 
-const Register = () => {
+const Register = ({
+  handleSubmit,
+  handleChange,
+  values,
+  handleImageChange,
+  handleImageUpload,
+  imageUrl,
+}) => {
   return (
     <div>
-      <form>
-        <TextField label="nombre y apellido" />
-        <TextField label="url image" />
-        <TextField label="email" />
-        <TextField label="password" />
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="nombre y apellido"
+          name="displayName"
+          onChange={handleChange}
+        />
+        <TextField label="email" name="email" onChange={handleChange} />
+        <TextField label="password" name="password" onChange={handleChange} />
+
+        {/* <input type="file" name="photoUrl" onChange={handleChange} /> */}
+        <div>
+          <input type="file" onChange={handleImageChange} />
+          <button onClick={handleImageUpload} type="button">
+            Upload Image
+          </button>
+          {imageUrl && <Image cloudName="di9tla2mu" publicId={imageUrl} />}
+        </div>
+
+        <button type="submit">Registrar</button>
       </form>
     </div>
   );

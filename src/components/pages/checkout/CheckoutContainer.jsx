e4779e82ec6d2ef89fa09f3812d20c3a59a092ac
Briansc7 <samudio.utn@gmail.com>
2023-07-28 15:30:30 -0300

    added mercado pago integration into checkout

--- src/components/pages/checkout/CheckoutContainer.jsx
+++ src/components/pages/checkout/CheckoutContainer.jsx
@@ -2,14 +2,57 @@ import Checkout from "./Checkout";
 
 import { checkoutInputsInfo } from "./CheckoutInputsInfo";
 
+import { useState } from "react";
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import axios from "axios";
+
 const CheckoutContainer = () => {
   const formInfo = {
     action: "/",
     method: "POST",
   };
+
+  const [preferenceId, setPreferenceId] = useState(null);
+  initMercadoPago(import.meta.env.VITE_PUBLICKEY);
+
+  const createPreference = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/create_preference",
+        [
+          {
+            title: "zapatilla puma",
+            unit_price: 5,
+            quantity: 3,
+          },
+          {
+            title: "zapatilla nike",
+            unit_price: 10,
+            quantity: 2,
+          },
+        ]
+      );
+
+      const { id } = response.data;
+      return id;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleBuy = async () => {
+    const id = await createPreference();
+    if (id) {
+      setPreferenceId(id);
+    }
+  };
+
   return (
     <div>
-      <Checkout formInfo={formInfo} checkoutInputsInfo={checkoutInputsInfo} />
+      {/* <Checkout formInfo={formInfo} checkoutInputsInfo={checkoutInputsInfo} /> */}
+
+      <button onClick={handleBuy}>Comprar</button>
+      {preferenceId && <Wallet initialization={{ preferenceId }} />}
     </div>
   );
 };
